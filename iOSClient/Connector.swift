f54caa346781d18b3329688a40d428a8dd9b2f28
Brandon Kong <shoukozumi@outlook.com>
2018-04-11 12:26:00 +0800

    Initial Commit

---
+++ iOSClient/Connector.swift
@@ -0,0 +1,41 @@
+//
+//  Connector.swift
+//  iOSClient
+//
+//  Created by Brandon Kong on 4/5/18.
+//  Copyright © 2018 Himokagami. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+let site_url = "http://dcszonline.dulwich-suzhou.cn/"
+let login_url = "login/index.php"
+let homework_url = "blocks/homework/view.php?course=1"
+let homeworkdata_url = "blocks/homework/ajax/view_timetable.php"
+let activity_url = "blocks/homework/assignment.php"
+
+let sessionManager = Alamofire.SessionManager.default
+
+class Connector{
+    init() {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        sessionManager.request(site_url + login_url, method: .post, parameters: ["username": "brandon.kong", "password": "***REMOVED***", "anchor": ""]).responseString { response in
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            print("Success: \(response.result.isSuccess)")
+            print("Response String: \(response.result.value ?? "")")
+            self.getHW()
+        }
+        
+        
+    }
+    
+    public func getHW(){
+        sessionManager.request(site_url + homework_url, method: .get).responseString { response in
+            print("Success: \(response.result.isSuccess)")
+            print("Response String: \(response.result.value ?? "")")
+        }
+    }
+    
+}
