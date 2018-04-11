9c4b24941c30938c95b4fd00e48a33546f8b4ec3
Brandon Kong <shoukozumi@outlook.com>
2018-04-11 22:06:53 +0800

    Removed

--- iOSClient/Connector.swift
+++ iOSClient/Connector.swift
@@ -22,7 +22,7 @@ let sessionManager = Alamofire.SessionManager.default
 var userid = 0
 var userRealName = ""
 var username = "brandon.kong"
-var password = "***REMOVED***"
+var password = ""
 
 class Connector{
     init() {
