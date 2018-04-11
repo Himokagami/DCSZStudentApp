a106d6e323c46a1a0a8b626378559c216537f220
Brandon Kong <shoukozumi@outlook.com>
2018-04-11 22:10:01 +0800

    Now private

--- iOSClient/Connector.swift
+++ iOSClient/Connector.swift
@@ -22,7 +22,7 @@ let sessionManager = Alamofire.SessionManager.default
 var userid = 0
 var userRealName = ""
 var username = "***REMOVED***"
-var password = ""
+var password = "***REMOVED***"
 
 class Connector{
     init() {
