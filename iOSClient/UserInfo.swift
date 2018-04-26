f6ef31b48d6910cc9ff4c028e58b27a6360a653f
Brandon Kong <shoukozumi@outlook.com>
2018-04-26 20:59:55 +0800

    Updated the UX design and completed the homework connector

---
+++ iOSClient/UserInfo.swift
@@ -0,0 +1,17 @@
+//
+//  UserInfo.swift
+//  iOSClient
+//
+//  Created by Brandon Kong on 4/22/18.
+//  Copyright © 2018 Himokagami. All rights reserved.
+//
+/*
+import UIKit
+
+class UserInfo: UITextField {
+
+    @IBOutlet weak var username: UITextField?
+    @IBOutlet weak var password: UITextField?
+    
+}
+*/
